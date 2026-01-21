9cb7df0a0883570e903fb17a0dbf66c34f0d397e
DuckThuong <trinhthuong26022003@gmail.com>
2026-01-21 21:46:23 +0700

    fix: build errorr

--- src/common/constants/regexs.ts
+++ src/common/constants/regexs.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-useless-escape
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// eslint-disable-next-line no-useless-escape
 export const PASSWORD_REGEX =
   /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+=\[\]{};':"\\|,.<>/?]).{8,}$/;
 export const OTP_REGEX = /^\d{4}$/;
