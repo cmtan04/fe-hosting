a084c3ee25e694b12cb020be48963bf6439e8832
DuckThuong <trinhthuong26022003@gmail.com>
2026-02-08 18:50:46 +0700

    fix: code

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,18 +1,29 @@
-import { Menu } from "antd";
-import { createSearchParams, Link } from "react-router-dom";
+import { Col, Menu, Row } from "antd";
+import { createSearchParams, Link, useNavigate } from "react-router-dom";
 import background from "../../assets/images/auth/authBackGround.jpg";
 import down from "../../assets/svg/icn-down_single.svg";
 import menu from "../../assets/svg/icn-menu.svg";
-import { USER_ROLE } from "../../common/constants/constants";
+import { NOTI_SUCCESS, USER_ROLE } from "../../common/constants/constants";
 import { ROUTER_PATH } from "../../router/Route";
 import "./topbar.scss";
 import { useState } from "react";
+import profile from "../../assets/images/profile/icn_profile.svg";
+import logout from "../../assets/images/profile/icn_logout.svg";
+import location from "../../assets/images/profile/icn_location.svg";
+import { useMutation } from "@tanstack/react-query";
+import { useNotification } from "../../providers/notificationProvider";
 
+interface ProfileItem {
+  key: number;
+  icon: any;
+  label: string;
+  href: string;
+}
 export const TopBar = () => {
-  // const navigate = useNavigate();
-  const [showProfile, setShowProfile] = useState<boolean>();
-
+  const navigate = useNavigate();
+  const { showNotification } = useNotification();
   const userRole = localStorage.getItem("userRole");
+  const [showProfile, setShowProfile] = useState<boolean>();
 
   const items = [
     {
@@ -182,9 +193,38 @@ export const TopBar = () => {
     },
   ];
 
-  const profileItems = [
-    { key: 1, icon: "", label: "Thông tin cá nhân", href: ROUTER_PATH.HOME },
+  const profileItems: ProfileItem[] = [
+    {
+      key: 1,
+      icon: profile,
+      label: "Thông tin cá nhân",
+      href: ROUTER_PATH.HOME,
+    },
+    {
+      key: 2,
+      icon: location,
+      label: "Địa điểm của tôi",
+      href: ROUTER_PATH.HOME,
+    },
+    {
+      key: 3,
+      icon: logout,
+      label: "Đăng xuất",
+      href: ROUTER_PATH.HOME,
+    },
   ];
+
+  const handleProfileClick = (data: ProfileItem) => {
+    if (data.key === 3) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("userRole");
+      showNotification("Đăng xuất thành công!", NOTI_SUCCESS);
+      navigate(ROUTER_PATH.SIGN_IN);
+    } else {
+      navigate(data.href);
+    }
+    setShowProfile(!showProfile);
+  };
 
   return (
     <div className="top__bar">
@@ -222,7 +262,24 @@ export const TopBar = () => {
           <img src={menu} alt="" className="menu" />
         </div>
 
-        {showProfile && <div className="profile__dropdown"></div>}
+        {showProfile && (
+          <div className="profile__dropdown">
+            {profileItems.map((item: ProfileItem) => (
+              <Row
+                gutter={[16, 16]}
+                className="profile__dropdown-item"
+                onClick={() => handleProfileClick(item)}
+              >
+                <Col span={4}>
+                  <img src={item.icon} alt={item.label} />
+                </Col>
+                <Col span={20}>
+                  <p>{item.label}</p>
+                </Col>
+              </Row>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
